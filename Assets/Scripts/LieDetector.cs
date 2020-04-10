09a3d21af72331cf3827bc50b1208796344ab14c
Jared Cosulich <jared.cosulich@gmail.com>
2020-04-10 14:43:54 -0400

    sranipal api and ability to change color

---
+++ Assets/Scripts/LieDetector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LieDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeColor(Color c)
+    {
+        List<Material> m = new List<Material>();
+        GameObject.Find("Sphere").GetComponent<Renderer>().GetMaterials(m);
+        m[0].SetColor("_Color", c);
+    }
+}
